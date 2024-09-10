b5d338f1b7717cb5b71540bc9be5325206760175
S0rrow <jae9hell@gmail.com>
2024-09-10 13:54:04 +0900

    [UPDATE] venv applied; deploy.sh repositioned

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 cd /home/ubuntu/TMI_web
 git pull origin main
+
+# virtual environemnt variable
+VENV_DIR="venv"
+
+# check if venv already exists
+if [ -d "$VENV_DIR"]; then
+  echo "Virtual Environment already exists. Updating dependencies..."
+  source $VENV_DIR/bin/activate
+else
+  echo "Virtual environment does not exist. Creating a new one..."
+  python3 -m venv $VENV_DIR
+  source $VENV_DIR/bin/activate
+fi
+
 pip install -r requirements.txt
 
 # Check if Streamlit is running on port 8501 and stop it
