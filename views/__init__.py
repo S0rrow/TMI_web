a21882baf2602302300b06d5cdb0a448655d5a0f
S0rrow <jae9hell@gmail.com>
2024-09-10 12:31:31 +0900

    [FIX] removed unused line

--- views/__init__.py
+++ views/__init__.py
@@ -4,5 +4,4 @@ from views.login import display_login_page
 from views.user_information import display_user_information
 from views.error import display_error_page
 from views.history import display_history
-from views.detail import display_job_information
 from views.utils import Logger
